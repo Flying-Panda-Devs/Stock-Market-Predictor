9d01f6927acb5628db26c62224a431506b42df96
HippoProgrammer <bcgcustomerservices@gmail.com>
2026-03-03 14:14:38 +0000

    Fix the installation script

--- installDependencies.sh
+++ installDependencies.sh
@@ -4,3 +4,4 @@ sudo apt install python python3-pip -y
 python -m venv stocks
 source stocks/bin/activate
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+pip3 install -r requirements.txt
