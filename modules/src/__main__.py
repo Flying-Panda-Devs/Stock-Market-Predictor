2c20528b7b756194b82c45d0d7703f767276268c
HippoProgrammer <bcgcustomerservices@gmail.com>
2026-03-24 14:24:33 +0000

    DATE BOF BOF

--- modules/src/__main__.py
+++ modules/src/__main__.py
@@ -121,10 +121,10 @@ active_model.eval()
 print('Model loaded. Ready for evaluation.')
 with torch.no_grad():
     if earliest < stock_date < latest:
-        new_input = torch.from_numpy(per_stock_data[stock_name].loc[per_stock_data[stock_name]['date'] == stock_date].to_numpy())
+        new_input = torch.from_numpy(per_stock_data[stock_name].loc[per_stock_data[stock_name]['date'] == stock_date].drop(columns=['date']).to_numpy())
         diff = 0
     else:
-        new_input = torch.from_numpy(per_stock_data[stock_name].loc[per_stock_data[stock_name]['date'] == latest].to_numpy())
+        new_input = torch.from_numpy(per_stock_data[stock_name].loc[per_stock_data[stock_name]['date'] == latest].drop(columns=['date']).to_numpy())
         diff = datetime.fromtimestamp(stock_date) - datetime.fromtimestamp(latest)
         diff = diff.days
     if diff != 0:
