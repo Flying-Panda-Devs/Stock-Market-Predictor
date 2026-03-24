1753ba645ad08ec519d28cdc80570b8a83ec7444
HippoProgrammer <bcgcustomerservices@gmail.com>
2026-03-24 13:57:42 +0000

    Remove date again

--- modules/src/__main__.py
+++ modules/src/__main__.py
@@ -40,8 +40,8 @@ per_stock_data = dict()
 print('Loading and transforming static data...')
 with alive_bar(len(stock_names)) as bar:
     for stock_name in stock_names:
-        per_stock_data[stock_name] = all_stock_data[all_stock_data['Name'] == stock_name].drop(columns=['Name','volume'])
-        per_stock_data[stock_name]['date'] = per_stock_data[stock_name]['date'].apply(np.vectorize(convertDateToInt))
+        per_stock_data[stock_name] = all_stock_data[all_stock_data['Name'] == stock_name].drop(columns=['Name','date','volume'])
+        #per_stock_data[stock_name]['date'] = per_stock_data[stock_name]['date'].apply(np.vectorize(convertDateToInt))
         per_stock_data[stock_name] = per_stock_data[stock_name].astype(np.float32)
         bar()
 tensors = dict()
@@ -51,7 +51,7 @@ print('Generating tensors...')
 with alive_bar(len(stock_names)) as bar:
     for stock_name in stock_names: # some of the tensors are weirdly small. check stock data.
         tensors['input'][stock_name] = torch.from_numpy(per_stock_data[stock_name].drop(per_stock_data[stock_name].tail(1).index).to_numpy())
-        tensors['output'][stock_name] = torch.from_numpy(per_stock_data[stock_name].drop(per_stock_data[stock_name].head(1).index).drop(columns=['date','high','low']).to_numpy())
+        tensors['output'][stock_name] = torch.from_numpy(per_stock_data[stock_name].drop(per_stock_data[stock_name].head(1).index).drop(columns=['high','low']).to_numpy())
         bar()
 print('Loading cached models...')
 models = dict()
@@ -59,7 +59,7 @@ with alive_bar(len(stock_names)) as bar:
     for stock_name in stock_names:
         if os.path.exists(f'working_data/trained_models/{stock_name}.pt'):
             model = nn.Sequential(
-                nn.Linear(5, 500),
+                nn.Linear(4, 500),
                 nn.BatchNorm1d(500),
                 nn.LeakyReLU(),
                 nn.Linear(500,120),
@@ -75,12 +75,13 @@ with alive_bar(len(stock_names)) as bar:
         bar()
 print('All cached models loaded.')
 stock_name = input('Stock name: ')
+stock_date = convertDateToInt(input('Target date (YYYY-MM-DD): '))
 print('Loading model...')
 if not (stock_name in list(models.keys())):
     print('Model not in cache.')
     # Define the model using nn.Sequential
     model = nn.Sequential(
-        nn.Linear(5, 500),
+        nn.Linear(4, 500),
         nn.BatchNorm1d(500),
         nn.LeakyReLU(),
         nn.Linear(500,120),
@@ -116,7 +117,7 @@ active_model = models[stock_name]
 active_model.eval()
 print('Model loaded. Ready for evaluation.')
 with torch.no_grad():
-    new_input = torch.tensor([[1360281600, 55.025001525878906,
+    new_input = torch.tensor([[55.025001525878906,
                         55.422500610351562,         54.209999084472656,
                         54.244998931884766]],dtype=torch.float32)
     prediction = active_model(new_input)
